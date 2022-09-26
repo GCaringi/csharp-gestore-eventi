aa87cfc33c093060366f46316e7962d070a926d6
GCaringi <giannicaringi94@gmail.com>
2022-09-26 10:51:07 +0200

    Milestone 2 With Exception

--- Event.cs
+++ Event.cs
@@ -19,23 +19,36 @@ public class Event
             _date = value;
         }
     }
-    public int Capacity { get; }
+    public int Capacity { get; private set; }
     public int Reserved { get; private set; }
 
     public Event(string title, DateOnly date, int capacity)
     {
+        if (title == "")
+        {
+            throw new ArgumentException("Title cannot be empty");
+        }
         Title = title;
         if (date < DateOnly.FromDateTime(DateTime.Now))
         {
             throw new ArgumentException("Date must be in the future");
         }
         _date = date;
+        if (capacity < 1)
+        {
+            throw new ArgumentException("Capacity must be greater than 0");
+        }
         Capacity = capacity;
         Reserved = 0;
     }
 
     public void Reserve(int seats)
     {
+        if (seats < 1)
+        {
+            throw new ArgumentException("Seats must be greater than 0");
+        }
+        
         if (seats > Capacity - Reserved)
         {
             throw new ArgumentException("Not enough seats available");
@@ -47,10 +60,16 @@ public class Event
         }
 
         Reserved += seats;
+        Capacity -= seats;
     }
     
     public void Cancel(int seats)
     {
+        if (seats < 1)
+        {
+            throw new ArgumentException("Seats must be greater than 0");
+        }
+        
         if (seats > Reserved)
         {
             throw new ArgumentException("Not enough seats reserved");
@@ -60,14 +79,16 @@ public class Event
         {
             throw new ArgumentException("Event already happened");
         }
-
+        
         Reserved -= seats;
+        Capacity += seats;
     }
     
     public override string ToString()
     {
         return $"{Date.ToString("dd/MM/yyyy")} - {Title}";
     }
+    
     
     
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,105 @@
 ﻿using csharp_gestore_eventi;
 
-DateOnly date = new DateOnly(2023,6, 9); // June 9, 2012
+Event defaultEvent = new Event("Convegno", new DateOnly(2023, 05, 12), 500);
 
-try
+// Event e = createEvent();
+Console.WriteLine(defaultEvent.ToString());
+
+// ReserveSeats(defaultEvent);
+// printInfo(defaultEvent);
+
+reserveAndCancel(defaultEvent);
+
+
+
+Event createEvent()
 {
-    Event e = new Event("Concerto", date, 500);
-    Console.WriteLine(e.Title);
-    Console.WriteLine(e.Date);
-    Console.WriteLine(e.Capacity);
-    Console.WriteLine(e.Reserved);
+    Event e = null;
+    try
+    {
+        Console.Write("Inserisci il nome dell'evento: ");
+        string title = Console.ReadLine() ?? "";
+        Console.Write("Inserisci la data dell'evento(gg/mm/aaaa): ");
+        DateOnly date = fromString(Console.ReadLine());
+        Console.Write("Inserisci la capienza dell'evento: ");
+        int capacity = Convert.ToInt32(Console.ReadLine() ?? "0");
+        e = new Event(title, date, capacity);
+    }
+    catch (ArgumentException error)
+    {
+        Console.WriteLine(error.Message);
+    }
+    
+    return e;
+}
+
+DateOnly fromString(string date)
+{
+    string[] dateArray = date.Split('/');
+    int day = Convert.ToInt32(dateArray[0]);
+    int month = Convert.ToInt32(dateArray[1]);
+    int year = Convert.ToInt32(dateArray[2]);
+    return new DateOnly(year, month, day);
+}
+
+void ReserveSeats(Event e)
+{
+    Console.Write("Quante posti vuoi prenotare? ");
+    int seats = Convert.ToInt32(Console.ReadLine() ?? "0");
+    try
+    {
+        e.Reserve(seats);
+    }
+    catch (ArgumentException error)
+    {
+        Console.WriteLine(error.Message);
+    }
+}
+
+void CancelSeats(Event e)
+{
+    Console.WriteLine("Quanti posti vuoi disdire: ");
+    int seats = Convert.ToInt32(Console.ReadLine() ?? "0");
+    try
+    {
+        e.Cancel(seats);
+    }
+    catch (ArgumentException error)
+    {
+        Console.WriteLine(error.Message);
+    }
+}
+
+void printInfo(Event e)
+{
+    Console.WriteLine($"Posti prenotati = {e.Reserved}");
+    Console.WriteLine($"Posti disponibili = {e.Capacity}");
+}
+
+void reserveAndCancel(Event e)
+{
+    printInfo(e);
+    ReserveSeats(e);
+    Console.Write("Vuoi disdire dei posti?(yes/no): ");
+    string prompt = Console.ReadLine() ?? "";
+
+    while (prompt == "yes")
+    {
+        try
+        {
+            CancelSeats(e);
+            printInfo(e);
+            
+            Console.Write("Vuoi disdire altri posti?(yes/no): ");
+            prompt = Console.ReadLine() ?? "";
+        }
+        catch (ArgumentException error)
+        {
+            Console.WriteLine(error.Message);
+        }
+    }
+    
+    Console.Clear();
     Console.WriteLine(e.ToString());
+    printInfo(e);
 }
-catch (ArgumentException e )
-{
-    Console.WriteLine(e.Message);
-}
-
