60a69086c6ce80df0dcfb8036d019df8b15faa6e
GCaringi <giannicaringi94@gmail.com>
2022-09-26 09:56:19 +0200

    Milestone 1

---
+++ Event.cs
@@ -0,0 +1,73 @@
+﻿namespace csharp_gestore_eventi;
+
+public class Event
+{
+    private DateOnly _date;
+    
+    public string Title { get; set; }
+
+    public DateOnly Date
+    {
+        get => _date;
+        set
+        {
+            if (value < DateOnly.FromDateTime(DateTime.Now))
+            {
+                throw new ArgumentException("Date must be in the future");
+            }
+
+            _date = value;
+        }
+    }
+    public int Capacity { get; }
+    public int Reserved { get; private set; }
+
+    public Event(string title, DateOnly date, int capacity)
+    {
+        Title = title;
+        if (date < DateOnly.FromDateTime(DateTime.Now))
+        {
+            throw new ArgumentException("Date must be in the future");
+        }
+        _date = date;
+        Capacity = capacity;
+        Reserved = 0;
+    }
+
+    public void Reserve(int seats)
+    {
+        if (seats > Capacity - Reserved)
+        {
+            throw new ArgumentException("Not enough seats available");
+        }
+        
+        if (DateOnly.FromDateTime(DateTime.Now) > Date)
+        {
+            throw new ArgumentException("Event already happened");
+        }
+
+        Reserved += seats;
+    }
+    
+    public void Cancel(int seats)
+    {
+        if (seats > Reserved)
+        {
+            throw new ArgumentException("Not enough seats reserved");
+        }
+        
+        if (DateOnly.FromDateTime(DateTime.Now) > Date)
+        {
+            throw new ArgumentException("Event already happened");
+        }
+
+        Reserved -= seats;
+    }
+    
+    public override string ToString()
+    {
+        return $"{Date.ToString("dd/MM/yyyy")} - {Title}";
+    }
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using csharp_gestore_eventi;
+
+DateOnly date = new DateOnly(2023,6, 9); // June 9, 2012
+
+try
+{
+    Event e = new Event("Concerto", date, 500);
+    Console.WriteLine(e.Title);
+    Console.WriteLine(e.Date);
+    Console.WriteLine(e.Capacity);
+    Console.WriteLine(e.Reserved);
+    Console.WriteLine(e.ToString());
+}
+catch (ArgumentException e )
+{
+    Console.WriteLine(e.Message);
+}
+
